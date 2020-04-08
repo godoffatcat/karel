c56f42978c2b2082dab8fe2abd214af4303a2e3d
fatcat <747061165@qq.com>
2020-04-08 20:31:42 +0800

    仓库更新

--- game.js
+++ game.js
@@ -1,66 +1,125 @@
 // var audio = document.getElementById('yepRight')
 // 图片导入
 var imageFromPath = function (path) {
-    var img = new Image()
-    img.src = path
-    console.log('skill path', img.src)
-    return img
-}
-console.log('引入图片地址', imageFromPath)
-
-// 图片加载
-// var drawPic = image.onload = function (image) {
-//     context.drawImage(image, 0, 0)
-// }
+  var img = new Image();
+  img.src = path;
+  console.log("skill path", img.src);
+  return img;
+};
+console.log("引入图片地址", imageFromPath);
 
 //技能(独立)
 // 随机出现的技能列表，也需要做多个坐标，以及图片导入。
-var Skill = function() {
-    var image = imageFromPath('skillimg/invoker_deafening.png')
-    console.log('imageskill', image)
-    var o = {
-        image : image,  
-        alive : true,
-        bgm: false,
-    }
-    image.onload = function () {
-        context.drawImage(o.image, 400, 250, 80, 80)
-    }
-}
- 
-var loadPic = function(image) {
-    context.drawImage(image, 300, 150, 80, 80)
-}
-// 当前输入的元素（独立）
-// 图片从左往右排列， 所以应该做的是3个坐标。todo
-var eleKeyQ = function() {
-    var image = imageFromPath('skillimg/invoker_ice.png')
-    console.log('image ice', image)
-    var o = {
-        image : image,  
-        alive : true,
-    }
-    loadPic(o.image)
-    // image.onload = function () {
-    //     context.drawImage(o.image, 300, 150, 80, 80)
-    // }
+var Skill = function () {
+  var image = imageFromPath("skillimg/invoker_deafening.png");
+  console.log("imageskill", image);
+  var o = {
+    image: image,
+    x: 400,
+    y: 250,
+    w: 80,
+    h: 80,
+    bgm: false,
+    displayFor: false,
+  };
+  // o.move = function() {
+  //     o.x -= 100
+  // }
 
-// 技能判断(监听)
-    window.addEventListener('keydown', event => {
-        if ([event.key] == 'q') {
-            console.log('keypressQ', image)
+  o.done = function () {
+    o.displayFor = true;
+  };
 
-        } else if ([event.key] == 'w') {
-            console.log('keypressW', image)
-        } else if ([event.key] == 'e') {
-            console.log('keypressE', image)
+  // move是为了让下一个技能左移，可是多个技能的多个坐标，需要放在一个仓库里吧？
+  // 按键也需要和当前技能匹配哦。
+  // 是不是需要每一个技能都需要有她独立的{}？
+
+  buttonS.onclick = function () {
+    o.done();
+    console.log("display?", o.displayFor);
+    loadPic(o.image, o.x, o.y, o.w, o.h);
+  };
+};
+
+// 当前输入的元素（独立）..
+// 如果技能正确，则alive回归false， 需要加个判断， 和skill联动
+var eleKey = function () {
+  var imageICE = imageFromPath(eleList.ice[0]);
+  var imageTHUNDER = imageFromPath(eleList.thunder[0]);
+  var imageFIRE = imageFromPath(eleList.fire[0]);
+
+  var ice = {
+    image: imageICE,
+    alive: false,
+  };
+
+  var thunder = {
+    image: imageTHUNDER,
+    alive: false,
+  };
+
+  var fire = {
+    image: imageFIRE,
+    alive: false,
+  };
+
+  // 技能判断(监听)
+  // 待修改： 坐标将以元素先后顺序为准。
+  const inputSkill = [];
+  window.addEventListener("keydown", (event) => {
+    // 第一个输入的元素将会删除，右边元素左移
+    if (inputSkill.length > 2) {
+            inputSkill.splice(0, 1);
         }
-    })
-}
 
-var Switch = function() {
-    Skill()
-    eleKeyQ()
-}
+        //按键判断区
+    if ([event.key] == "q") {
+      loadPic(ice.image, 100, 200, 40, 40);
+      ice.alive = true;
+      inputSkill.push("q");
+      console.log('当前输入', inputSkill)
+    } else if ([event.key] == "w") {
+      loadPic(thunder.image, 160, 200, 40, 40);
+      thunder.alive = true;
+      inputSkill.push("w");
+      console.log('当前输入', inputSkill)
+    } else if ([event.key] == "e") {
+      loadPic(fire.image, 220, 200, 40, 40);
+      fire.alive = true;
+      inputSkill.push("r");
+      console.log('当前输入', inputSkill)
+    }
 
-Switch()
+  });
+
+};
+
+// var judge = function () {
+//   let a = skillList.skill01;
+//   let b = inputSkill;
+//   if (a.length === b.length) {
+//     for (let i = 1; i < a.length; i++) {
+//       for (let j = 0; j < b.length; j++) {
+//         if (b[j] === a[i]) {
+//           b = b.filter(function (v) {
+//             return v !== b[j];
+//           });
+//         }
+//       }
+//     }
+//     yepskill()
+// } 
+// };
+
+// window.addEventListener("keydown", (event) => {
+//   if ([event.key] == "r") {
+//     judge();
+//   }
+// });
+
+Switch = function () {
+  Skill();
+  eleKey();
+};
+
+Switch();
---
+++ house.js
@@ -0,0 +1,17 @@
+// 技能的图片位置、匹配按键
+var skillList = {
+        skill01 : ['path.jpg' , 'q', 'w', 'e'],
+        skill02 : ['path.jpg' , 'q', 'q', 'e'],
+        skill03 : ['path.jpg' , 'q', 'e', 'e'],
+        skill04 : ['path.jpg' , 'w', 'w', 'e'],
+        skill05 : ['path.jpg' , 'w', 'e', 'e'],
+}
+
+console.log('path test', skillList.skill01[0])
+
+// 按键的图片位置、音效
+var eleList = {
+    ice : ['skillimg/invoker_ice.png', 'bgm'],
+    thunder : ['skillimg/invoker_thunder.png', 'bgm'],
+    fire : ['skillimg/invoker_fire.png', 'bgm'],
+}
