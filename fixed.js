d324db44ab7e5948732632334e9489f64f7a7a3c
fatcat <747061165@qq.com>
2020-04-08 20:31:28 +0800

    仓库

---
+++ fixed.js
@@ -0,0 +1,55 @@
+// 图标位置的定位坐标、宽高
+// 按下按键之后要在这些坐标上画图
+var eleInput = function(type) {
+    var 
+}
+
+// 这里重复的坐标或许可以用继承？
+var eleFix = function(y, w, h){
+    firstEle = {
+        x : 200,
+        y : y, 
+        w: w,
+        h: h,
+    },
+
+    secondEle = {
+        x : 260,
+        y : y, 
+        w: w,
+        h: h,
+    },
+
+    thirdEle = {
+        x : 320,
+        y : y, 
+        w: w,
+        h: h,
+    },
+}
+
+eleFix(150, 40, 40)
+
+// 技能图标的定位坐标、宽高
+var skillFix = {
+    firstEle = {
+        x : 200,
+        y : 100, 
+        w: 40,
+        h: 40,
+    },
+
+    secondEle = {
+        x : 260,
+        y : 100, 
+        w: 40,
+        h: 40,
+    },
+
+    thirdEle = {
+        x : 320,
+        y : 100, 
+        w: 40,
+        h: 40,
+    },
+}
---
+++ loadPicture.js
@@ -0,0 +1,15 @@
+// 纯函数，仅提供写入读取和渲染的图片区。
+
+var imageFromPath = function (path) {
+    var img = new Image()
+    img.src = path
+    console.log('skill path', img.src)
+    return img
+}
+// console.log('引入图片地址', imageFromPath)
+
+var loadPic = function(image, x, y, w, h) {
+    
+    context.drawImage(image, x, y, w, h)
+}
+
